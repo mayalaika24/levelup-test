19482e00e2e8e49e95e210a74bbf0d6d3cae07b9
mayalaika98 <mayalaika98@gmail.com>
2026-03-09 21:45:18 +0300

    build

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ locale: "en" | "ar" }>;
+  params: Promise<{ locale: string }>;
 }>) {
   const { locale } = await params;
   setRequestLocale(locale);
